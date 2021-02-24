d37af36176c6b213c890cb8dca2cbbd36c35679f
josura <giorgiolocicero@hotmail.com>
2021-02-24 15:08:54 +0100

    Code cleaner and test

--- mining/generalframework/src/main/scala/infrastructure/CodeCleaner.scala
+++ mining/generalframework/src/main/scala/infrastructure/CodeCleaner.scala
@@ -2,11 +2,24 @@ package infrastructure
 
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.functions.length
+import org.apache.spark.sql.functions.{length,udf}
+import scala.io.Source
 
 object  CodeCleaner {
 
-    val symbolFile = "data/javaSyntax.txt"
+    //val symbolFile = "/opt/mining/data/javaSyntax.txt"
+    val symbolFile = "../data/javaSyntax.txt"
+
+    val symbols = Source.fromFile(symbolFile).getLines.mkString.split("[ ,.\"]").filter((str)=> !str.isEmpty)
+
+    def cleanCodeString(str:String):String = {
+        str.split("[ ,.\"();]").filter((str)=> !str.isEmpty && symbols.contains(str)).mkString(" ")
+    }
+
+    val cleaner = udf((code:String)=> {
+
+        cleanCodeString(code);
+    })
 
     def cleanCode(data:Dataset[Row]):Dataset[Row] = {
         if(data.isEmpty){
@@ -14,9 +27,9 @@ object  CodeCleaner {
             return data
         }
         ClassifierLogger.printInfo(" cleaning code ")
-
-
-
-        data.na.drop.filter(length(data("code"))>3)
+        var cleanedata = data.withColumn("cleanedCode",cleaner(data("code")))
+        cleanedata.na.drop
     }
+
+    
 }
