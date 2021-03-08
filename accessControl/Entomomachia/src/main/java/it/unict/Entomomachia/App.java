9989a78eea971a958346669ffec49fc8937e742b
josura <giorgiolocicero@hotmail.com>
2021-03-08 22:57:15 +0100

    started project for access control in java

---
+++ accessControl/Entomomachia/src/main/java/it/unict/Entomomachia/App.java
@@ -0,0 +1,13 @@
+package it.unict.Entomomachia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Access control started" );
+    }
+}
