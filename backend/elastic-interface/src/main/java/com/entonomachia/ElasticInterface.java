0bbe73604de4e7b03cee697234585f456e7bf75e
josura <giorgiolocicero@hotmail.com>
2021-09-26 09:37:03 +0200

    changed idea and only doing the elastic interface with POJO and not spring for simplicity

--- backend/elastic-interface/src/main/java/com/entonomachia/ElasticInterface.java
+++
@@ -1,8 +0,0 @@
-package com.entonomachia;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ElasticInterface extends Remote {
-	public QueryResult getResult() throws RemoteException;
-}
